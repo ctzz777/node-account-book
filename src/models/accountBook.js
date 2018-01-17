2d90125e09ceff5d938f74aa7685804328ca4ee4
CTzz <ctzz@protonmail.com>
2018-01-18 00:47:35 +0800

    Add commons and routines to AccountBook schema

--- src/models/accountBook.js
+++ src/models/accountBook.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const AccountBook = new Schema({
   userId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   budget: {type: Number, default: 0},
+  commons: [{type: Schema.Types.ObjectId, ref: 'AccountElement', required: true}],
+  routines: [{type: Schema.Types.ObjectId, ref: 'Routine', required: true}],
 });
 
 module.exports = mongoose.model('AccountBook', AccountBook, 'AccountBook');
