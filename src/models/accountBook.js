5baac65acff4f194226ae7ba65921d501bc4a8b2
CTzz <ctzz@protonmail.com>
2018-02-28 02:27:36 +0800

    Integrate accountTemplate and routine schema to AccountBook

--- src/models/accountBook.js
+++ src/models/accountBook.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
-const accountTemplateSchema = require('./schemas/accountTemplate');
 const Schema = mongoose.Schema;
+
+const AccountTemplate = new Schema({
+  category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
+  description: {type: String, default: ''},
+  amount: {type: Number, default: 0},
+});
+
+const Routine = new Schema({
+  accountTemplate: {type: AccountTemplate, required: true},
+  frequency: [{type: String, enum: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']}],
+});
 
 const AccountBook = new Schema({
   userId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   budget: {type: Number, default: 15000},
-  commons: [{type: accountTemplateSchema, required: true}],
-  routines: [{type: Schema.Types.ObjectId, ref: 'Routine', required: true}],
+  commons: [{type: AccountTemplate, required: true}],
+  routines: [{type: Routine, required: true}],
 });
 
 module.exports = mongoose.model('AccountBook', AccountBook, 'AccountBook');
