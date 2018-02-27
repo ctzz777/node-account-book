831c6f2f7477bbb59315377f2e1005a672405555
CTzz <ctzz@protonmail.com>
2018-02-28 01:32:57 +0800

    AccountTemplate

--- src/models/accountBook.js
+++ src/models/accountBook.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const AccountBook = new Schema({
   userId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   budget: {type: Number, default: 15000},
-  commons: [{type: Schema.Types.ObjectId, ref: 'AccountElement', required: true}],
+  commons: [{type: Schema.Types.ObjectId, ref: 'AccountTemplate', required: true}],
   routines: [{type: Schema.Types.ObjectId, ref: 'Routine', required: true}],
 });
 
