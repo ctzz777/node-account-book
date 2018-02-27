a56fb18f9e7ac9e31d8e9b3f7b66f990e47951d6
CTzz <ctzz@protonmail.com>
2018-02-28 00:26:34 +0800

    Add account date validator

--- src/models/account.js
+++ src/models/account.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 const Schema = mongoose.Schema;
 
 const Account = new Schema({
   accountBookId: {type: Schema.Types.ObjectId, ref: 'AccountBook', required: true},
-  date: {type: String, required: true},
+  date: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return moment(v, "YYYYMMDD").isValid();
+      },
+      message: '{VALUE} is not in the valid format: YYYYMMDD'
+    }, 
+    required: true
+  },
   category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
   description: {type: String, default: ''},
   amount: {type: Number, default: 0},
