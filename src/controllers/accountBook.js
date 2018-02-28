09b70f84a0f72c4cbd7db69ce9f1960c6f57412d
CTzz <ctzz@protonmail.com>
2018-02-28 23:00:57 +0800

    Find accountBook by userId in token

--- src/controllers/accountBook.js
+++ src/controllers/accountBook.js
@@ -5,6 +5,20 @@ class AccountBookController {
     const accountBooks = await AccountBook.find({});
     ctx.body = accountBooks;
   }
+
+  async findByUser(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      ctx.throw(401);
+    }
+    const userId = user._id;
+    const accountBook = await AccountBook.findOne({userId});
+    if (!accountBook) {
+      ctx.throw(404);
+    }
+    ctx.body = accountBook;
+
+  }
 }
 
 module.exports = new AccountBookController();
--- src/routes/api/accountBook.js
+++ src/routes/api/accountBook.js
@@ -3,5 +3,6 @@ const accountBookController = require('../../controllers/accountBook');
 const router = new Router();
 
 router.get('/', accountBookController.findAll);
+router.get('/mine', accountBookController.findByUser);
 
 module.exports = router;
