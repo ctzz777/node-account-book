75bb629549d9a20b464e0d1b539e56bea6ddc44a
CTzz <ctzz@protonmail.com>
2018-03-21 21:46:21 +0800

    AccountItem and fix required array

--- src/models/accountBook.js
+++ src/models/accountBook.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const AccountTemplate = new Schema({
+const AccountItem = new Schema({
   category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
   description: {type: String, default: ''},
   amount: {type: Number, default: 0},
 });
 
 const Routine = new Schema({
-  accountTemplate: {type: AccountTemplate, required: true},
+  accountItem: {type: AccountItem, required: true},
   frequency: [{type: String, enum: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], required: true}],
 });
 
 const AccountBook = new Schema({
   userId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   budget: {type: Number, default: 15000},
-  commons: [{type: AccountTemplate, required: true}],
-  routines: [{type: Routine, required: true}],
+  commons: [{type: AccountItem}],
+  routines: [{type: Routine}],
 });
 
 module.exports = mongoose.model('AccountBook', AccountBook, 'AccountBook');
