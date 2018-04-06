864aabf674972600c3020c710842e64a61016af3
CTzz <ctzz@protonmail.com>
2018-04-06 22:14:11 +0800

    Set mongoose connectTimeout

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.plugin(require('./plugins/baseSchemaPlugin'));
-mongoose.connect(process.env.DB_URI, { useMongoClient: true });
+mongoose.connect(process.env.DB_URI, { useMongoClient: true, connectTimeoutMS: 3000 });
 mongoose.Promise = global.Promise;
 
 exports.mongoose = mongoose;
